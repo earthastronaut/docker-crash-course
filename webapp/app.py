bc5e94cd506c3232b2a10a180f5111694e377550
earthastronaut <>
2020-04-01 20:07:28 -0600

    fixed ports for webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+
+PORT = os.environ['PORT']
 
 
 @app.route("/")
@@ -8,4 +11,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=PORT)
